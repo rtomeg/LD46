cade7520c0ccdb2886c4a37bc4aa178f809fefd8
Rocio Tome <rtomegonzalez@gmail.com>
2020-04-19 12:43:52 +0200

    DG fix & nokia idle & shake animation

--- Assets/NokiaController.cs
+++ Assets/NokiaController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using DG.Tweening;
+using UnityEngine;
 
 public class NokiaController : MonoBehaviour
 {
+    Tween iddle;
+
+    void Start(){
+        iddle = transform.DORotate(new Vector3(transform.rotation.x, transform.rotation.y, -10), 3).SetLoops(-1, LoopType.Yoyo);
+    }
     public void Shake(){
-        
+        //myShake.Restart();
+        transform.DOShakePosition(0.1f, 1);
     }
 }
