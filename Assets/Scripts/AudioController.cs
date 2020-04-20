ae25a22208e29d459707192b53faa7107941c2e3
Rocio Tome <rtomegonzalez@gmail.com>
2020-04-20 02:37:34 +0200

    add music and final touchs

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -10,18 +10,76 @@ public class AudioController : MonoBehaviour
     AudioClip endCall;
     [SerializeField]
     AudioClip click;
+    [SerializeField]
     AudioSource sfx;
-    void Awake(){
-        sfx = GetComponent<AudioSource>();
+
+    [SerializeField]
+    List<AudioClip> musicList;
+
+    [SerializeField]
+    AudioSource music;
+
+    private bool activeMusic = true;
+
+    public static AudioController instance;
+
+    #region SINGLETON PATTERN 
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
     }
-    public void PlayBeepKey(){
+    #endregion
+
+    void Start()
+    {
+        StartMusic();
+    }
+
+    public void MusicIconClick()
+    {
+        if (activeMusic)
+        {
+            StopAllCoroutines();
+            music.Stop();
+            activeMusic = false;
+        }
+        else
+        {
+            StartMusic();
+            activeMusic = true;
+        }
+    }
+    void StartMusic()
+    {
+        StartCoroutine(MusicCorroutine());
+    }
+
+    IEnumerator MusicCorroutine()
+    {
+        music.clip = musicList[Random.Range(0, musicList.Count)];
+        music.Play();
+        yield return new WaitForSeconds(30);
+        MusicCorroutine();
+    }
+
+    public void PlayBeepKey()
+    {
         sfx.PlayOneShot(beepKey, 0.2f);
     }
-    public void PlayEndCallSound(){
+    public void PlayEndCallSound()
+    {
         sfx.PlayOneShot(endCall, 0.2f);
     }
 
-    public void PlayClickSound(){
+    public void PlayClickSound()
+    {
         sfx.PlayOneShot(click, 0.2f);
     }
 }
